f4aaefea9592fbb1e8b7def9d2437594c9d4c345
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-07-08 16:22:43 -0400

    added expenses component to app

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,38 +1,37 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import './Expenses.css'
 function Expenses(props){
 return(
-<div>
-    <div className="expenses ">
+<Card>
+    
     <ExpenseItem
     title={props.expense[0].title}
     amount={props.expense[1].amount}
     date={props.expense[0].date}
   ></ExpenseItem>
-</div>
-<div className="expenses ">
+
+
   <ExpenseItem
     title={props.expense[1].title}
     amount={props.expense[1].amount}
     date={props.expense[1].date}
   ></ExpenseItem>
-</div>
-<div className="expenses ">
+
+
   <ExpenseItem
     title={props.expense[2].title}
     amount={props.expense[2].amount}
     date={props.expense[2].date}
   ></ExpenseItem>
-  </div>
+  
 
-<div className="expenses ">
   <ExpenseItem
     title={props.expense[3].title}
     amount={props.expense[3].amount}
     date={props.expense[3].date}
   ></ExpenseItem>
-  </div>
-</div> )
+</Card> )
 
 }
 
