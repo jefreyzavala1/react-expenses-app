4a7396a2d7576036f7f1f1ba992b0e202f61cdbe
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-07-10 00:50:46 -0400

    added state to app

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,39 +1,35 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import './Expenses.css'
-const Expenses = props=>{
-return(
-<Card>
-    
-    <ExpenseItem
-    title={props.expense[0].title}
-    amount={props.expense[1].amount}
-    date={props.expense[0].date}
-  ></ExpenseItem>
+import "./Expenses.css";
+const Expenses = (props) => {
+  return (
+    <Card>
+      <ExpenseItem
+        title={props.expense[0].title}
+        amount={props.expense[1].amount}
+        date={props.expense[0].date}
+      ></ExpenseItem>
 
+      <ExpenseItem
+        title={props.expense[1].title}
+        amount={props.expense[1].amount}
+        date={props.expense[1].date}
+      ></ExpenseItem>
 
-  <ExpenseItem
-    title={props.expense[1].title}
-    amount={props.expense[1].amount}
-    date={props.expense[1].date}
-  ></ExpenseItem>
+      <ExpenseItem
+        title={props.expense[2].title}
+        amount={props.expense[2].amount}
+        date={props.expense[2].date}
+      ></ExpenseItem>
 
-
-  <ExpenseItem
-    title={props.expense[2].title}
-    amount={props.expense[2].amount}
-    date={props.expense[2].date}
-  ></ExpenseItem>
-  
-
-  <ExpenseItem
-    title={props.expense[3].title}
-    amount={props.expense[3].amount}
-    date={props.expense[3].date}
-  ></ExpenseItem>
-</Card> )
-
-}
+      <ExpenseItem
+        title={props.expense[3].title}
+        amount={props.expense[3].amount}
+        date={props.expense[3].date}
+      ></ExpenseItem>
+    </Card>
+  );
+};
 
 export default Expenses;
