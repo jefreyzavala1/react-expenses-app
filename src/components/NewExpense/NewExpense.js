716b0dc7703786e6dac2ffc729e43e0a2ebb67a1
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-07-11 23:41:32 -0400

    added form component to app

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,12 @@
 import React from "react";
-import './NewExpense.css';
+import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
-const NewExpense = ()=>{
-    return <div className="new-expense">
-    <ExpenseForm/>
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
     </div>
-
-}
+  );
+};
 
 export default NewExpense;
