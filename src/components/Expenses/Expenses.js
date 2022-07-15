d1d3a92f931fdf1b4c27a3660f6ff66a7554478a
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-07-14 20:54:02 -0400

    added filter year functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 const Expenses = (props) => {
+  //store picked year here in state?
+  //listen to date picked then forward date to here
+  const [filteredYear,setFilteredYear] = useState('2020');
+  const filterChangeHandler = selectedYear =>{
+       setFilteredYear(selectedYear);
+
+  }
   return (
-    <Card>
+    
+    <div>
+    <Card className = 'expenses'>
+    <ExpensesFilter selected = {filteredYear} onFilterChangeHandler={filterChangeHandler}/>
       <ExpenseItem
         title={props.expense[0].title}
         amount={props.expense[1].amount}
@@ -29,6 +40,7 @@ const Expenses = (props) => {
         date={props.expense[3].date}
       ></ExpenseItem>
     </Card>
+    </div>
   );
 };
 
