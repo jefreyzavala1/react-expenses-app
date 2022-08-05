f237aa4ba73849c6c66129eb27147049d308dc9a
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-08-05 15:06:54 -0400

    finish app

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,13 +2,13 @@ import React from "react";
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 const Chart = (props) => {
-    const dataPointValues  = props.dataPoints.map(dataPoint => dataPoint.value);
-    const totalMaximum = Math.max(...dataPointValues);
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
-        key = {dataPoint.label}
+          key={dataPoint.label}
           value={dataPoint.value}
           maxValue={totalMaximum}
           label={dataPoint.label}
