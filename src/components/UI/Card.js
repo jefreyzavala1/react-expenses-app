f4af4fc50f03d3ca5cf218be5193cfc98ed500bc
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-11-21 19:44:46 -0500

    added card style

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Card.css";
+//concept of composition(children props)
+//when combining components you used composotion
+//children props allows to create wrapper components
 const Card = (props) => {
   const classes = "card " + props.className;
   return <div className={classes}>{props.children}</div>;
